e5991d58d17bd0361faf0c1bac7bd95a63cc08dc
lumilio <logistic-division@lumilium.com>
2021-12-08 20:13:55 +0100

    remake live 2

--- blocco 2/live 2/index.php
+++ blocco 2/live 2/index.php
@@ -1,1 +1,17 @@
 <!-- Stampare a schermo, attraverso il foreach, tutti gli elementi passati in GET. -->
+
+
+
+<?php
+
+
+foreach($_GET as $key => $value) {
+  echo "<p>{$key} : {$value}</p>";
+}
+
+?>
+
+
+
+
+
